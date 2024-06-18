bf29a20145f3234a4945253b581129d983a81bcf
Alan Cavalcante <alanmaltacavalcante@gmail.com>
2024-06-17 21:32:24 -0300

    replace avatar, links and names

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/myavatar-light.png")
     img.setAttribute("alt", "Foto Perfil light")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/myavatar.png")
     img.setAttribute("alt", "Foto Perfil dark")
   }
 }
